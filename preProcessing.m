fdcb49fa6a740d7373de912a1196fedbb253a32e
Ansuya Ahluwalia <ansuya@s-169-232-191-19.resnet.ucla.edu>
2014-12-01 18:00:30 -0800

    save all preprocessed and raw training images as jpg

--- preProcessing.m
+++ preProcessing.m
@@ -1,25 +1,33 @@
-trainingImagePath = 'C:\Users\Nicholas\Visual Modelling\train-images.idx3-ubyte';
-trainingLabelPath = 'C:\Users\Nicholas\Visual Modelling\train-labels.idx1-ubyte';
-numToTrain = 20;
+trainingImagePath = '/Users/ansuya/Documents/cs269-proj/mnist/train-images-idx3-ubyte';
+trainingLabelPath = '/Users/ansuya/Documents/cs269-proj/mnist/train-labels-idx1-ubyte';
+%can set to 60,000 for all training examples and 10,000 for all testing
+%examples
+numToTrain = 60000;
 offset = 0;
 
 [imgs, labels] = readMNIST(trainingImagePath, trainingLabelPath, numToTrain, offset);
-image = imgs(:,:,1);
+for i = 1:numToTrain
+image = imgs(:,:,i);
 
 level = graythresh(image);
 
-subplot(1,3,1)
-imshow(image);
-title('Before Otsu Thresholding');
+%subplot(1,3,1)
+%imshow(image);
+%title('Before Otsu Thresholding');
+%mkdir(/Users/ansuya/Documents/cs269-proj/','training-raw');
+imwrite(image, strcat('/Users/ansuya/Documents/cs269-proj/training-raw/img', num2str(i), '.jpg'));
 
 image(image < level) = 0;
 image(image ~= 0) = 1;
 
-subplot(1,3,2)
-imshow(image);
-title('After Thresholding');
+%subplot(1,3,2)
+%imshow(image);
+%title('After Thresholding');
 
 image = bwmorph(image,'skel',Inf);
-subplot(1,3,3)
-imshow(image);
-title('After Thinning/Skeletonization');
+%subplot(1,3,3)
+%imshow(image);
+%title('After Thinning/Skeletonization');
+%mkdir(/Users/ansuya/Documents/cs269-proj/','training-processed');
+imwrite(image, strcat('/Users/ansuya/Documents/cs269-proj/training-processed/img', num2str(i), '.jpg'));
+end
