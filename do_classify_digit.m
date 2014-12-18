1f7a30a1ecb8b45bd4b5cce79ac79c12aa33a347
Eric Kim <erickim555@gmail.com>
2014-12-18 04:34:50 -0800

    Minor changes to do_classify_digit.m (more visuals, save workspace, save diary, etc.). Added an alternate classification method that simply adds more weight to the E_fit term.

--- do_classify_digit.m
+++ do_classify_digit.m
@@ -24,11 +24,15 @@ if (~exist('FLAG_GS', 'var'))
     N_0 = 160; % "Standard" nb. of pixels. [See pg. 31 of thesis]
     PI_N = 0.3; % mixing coef. btwn uniform-noise and gaussian-mixture
     C = 1.0; % weight of E_def
-    LAMBDA_REG = 10.0; % regularization param for min_E_def()
+    LAMBDA_REG = 0.0; % regularization param for min_E_def()
+    P_A = 5.0; % for classification: E_tot = E_def + P_A*E_fit
     VERBOSE = 1;
     RNG_SEED = 42;
 end
-
+if 1
+    tmp_fpath = sprintf('%s_diary.txt', datestr(now, 'dd-mmm-yyyy_HH_MM_SS'));
+    diary(tmp_fpath);
+end
 %% Read Images
 rng(RNG_SEED);
 trainingImagePath = 'mnist/train-images.idx3-ubyte';
@@ -36,19 +40,37 @@ trainingLabelPath = 'mnist/train-labels.idx1-ubyte';
 numToTrain = 10000;
 offset = 0;
 
-%inds_2 = [6, 17, 26, 29, 77, 83, 110, 118, 121, 123, 144];
-%inds_3 = [8, 11, 13, 28, 45, 50, 51, 75, 87];
-
 [imgs, labels] = readMNIST(trainingImagePath, trainingLabelPath, numToTrain, offset);
+%% Decide which images to use
 inds_2 = find(labels==2)';
 inds_3 = find(labels==3)';
-NB_EXS = 50;
+NB_EXS = 200;
 inds_2 = inds_2(randperm(length(inds_2), NB_EXS/2));
 inds_3 = inds_3(randperm(length(inds_3), NB_EXS/2));
-
 inds_imgs = [inds_2, inds_3];
+
 %inds_imgs = [199];
-inds_imgs = inds_imgs(10);
+%inds_imgs = inds_imgs(10);
+
+% A few 'easy' examples
+%inds_2 = [6, 17, 26, 29, 77, 83, 110, 118, 121, 123, 144];
+%inds_3 = [8, 11, 13, 28, 45, 50, 51, 75, 87];
+%inds_imgs = [inds_2, inds_3];
+
+%inds_imgs = [26]; % an easy 2
+%inds_imgs = [77]; % an easy 2
+%% Visualize the images
+if 0
+    hfig = figure;
+    for ind=inds_imgs
+        I = imresize(squeeze(imgs(:,:,ind)), 5.0);
+        imshow(I);
+        title(sprintf('Img ID: %d', ind));
+        pause;
+    end
+    close(hfig);
+end
+
 %% Define Models
 % Digit model 2
 twoCPs = [[1.5, 3];
@@ -82,6 +104,38 @@ models = {...
     {3, threeCPs'}, ...
     };
 
+if (~exist('FLAG_GS', 'var'))
+    %% Visualize spline models
+    hfig = figure;
+    for i=1:length(models)
+        % Compute affine-trans that maps spline-ctrl-pts to fill up image
+        ps = models{i}{2}; % [2 x N]
+        x1 = min(ps(1,:));
+        y1 = min(ps(2,:));
+        x2 = max(ps(1,:));
+        y2 = max(ps(2,:));
+        w = x2 - x1; h = y2 - y1;
+        bbox_obj = [[x1 y1];
+            [x1+w, y1];
+            [x1, y1+h];
+            [x1+w, y1+h]];
+        bbox_img = [[0 0];
+            [20, 0];
+            [0, 20];
+            [20, 20]];
+        tform = cp2tform(bbox_obj, bbox_img, 'affine');
+        % Note: T is arranged in a weird way (transpose it to get familiar affine)
+        T = tform.tdata.T'; % transpose
+        tmpA = T(1:2, 1:2);
+        tmpt = T(1:2, 3);
+        ps_ = tmpA*ps + repmat(tmpt, [1, size(ps, 2)]);
+        subplot(1, length(models), i);
+        Ishow = rasterize_spline(ps_, [20,20]);
+        imshow(Ishow);
+        title(sprintf('"%d" model', models{i}{1}));
+    end
+end
+
 %% Preprocess imgs
 imgs_p = [];
 for i=1:length(inds_imgs)
@@ -106,6 +160,14 @@ end
 toc1 = toc(tic1);
 fprintf('Done fitting. (%.2fs)\n', toc1);
 
+%% Save workspace to file.
+if 1
+tmp_fpath = sprintf('%s.mat', datestr(now, 'dd-mmm-yyyy_HH_MM_SS'));
+clear('imgs');
+save(tmp_fpath);
+fprintf('Saved results to: %s\n', tmp_fpath);
+end
+
 %% Evaluate
 nb_pos = sum(labels(inds_imgs) == labels_pred');
 nb_neg = length(inds_imgs) - nb_pos;
@@ -126,6 +188,27 @@ for i=1:length(models)
     fprintf('[Digit %d] Acc: %.4f (nb_pos: %d nb_neg: %d)\n', digit_cur, acc_dig, nb_pos_dig, nb_neg_dig);
 end
 fprintf('Done.\n');
+
+%% Do alternate classification
+labels_pred2 = [];
+for i=1:length(intermedss) % for each image
+    label_pred = labels_pred(i);
+    label_true = labels(inds_imgs(i));
+    Es = [];
+    for m_i=1:length(intermedss{i})
+        % {xs_est, A, t, E_def, E_fit, N_B}
+        res = intermedss{i}{m_i}{end};
+        Es(m_i) = res{4} + P_A*res{5};
+    end
+    [~, ind_pred] = min(Es);
+    labels_pred2(i) = models{ind_pred}{1};
+end
+nb_pos2 = sum(labels(inds_imgs) == labels_pred2');
+nb_neg2 = length(inds_imgs) - nb_pos2;
+acc2 = nb_pos2 / (nb_pos2 + nb_neg2);
+accs_total2 = [acc2, nb_pos2, nb_neg2, P_A];
+fprintf('Alternate Classification (P_A=%f):\n', P_A);
+fprintf('    Acc: %.4f (nb_pos: %d nb_neg: %d)\n', acc2, nb_pos2, nb_neg2);
 
 if ~exist('FLAG_GS', 'var') % don't visualize if we're doing gridsearch
 %% Visualize classifications
@@ -159,7 +242,7 @@ end
 end
 
 if 0
-%% Visualize
+%% Visualize iterations
 for i = 1 : length(params_pred) % for each image
     % Visualize final output
     xs = params_pred{i}{1};
